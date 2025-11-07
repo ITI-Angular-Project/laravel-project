d292c8b9bc586c6f61cc104daed3d4ee6e33d88e
Ahmed Alaa <ahmed.alla56756@gmail.com>
2025-11-07 20:27:38 +0200

    ensure the user id in the rolemiddleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -2,20 +2,21 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
 {
     /**
      * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        $user = $request->user();
+        $user = User::find(Auth::id());
+
         if (!$user || !$user->hasRole($roles)) {
             abort(403, 'You do not have permission to access this resource.');
         }
